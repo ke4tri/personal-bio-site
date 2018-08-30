0d3d7be9749b9f88664c6e629c86e9f1ea17a6b6
Wayne Collier <ke4tri@gmail.com>
2018-08-30 06:37:46 -0500

    missed projects.js commit

--- projects.js
+++ projects.js
@@ -1,16 +1,41 @@
-projects = []
-
-
-
-
-
-
-
-
-
-
+const projects = 
+[
+    {
+    title: "Cool Project", 
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+}
+];
 
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += stringToPrint;
 };
+
+const createProjectCards = () => {
+    let newString = '';
+    for(i=0;i<projects.length;i++){
+        if(projects[i].available === true) {
+        newString += `<div class="projectCard">`;
+        newString +=    `<h3>${projects[i].title}</h3>`;
+        newString +=    `<img src="${projects[i].screenshot}">`;
+        newString +=    `<h3>${projects[i].description}</h3>`;
+        newString +=    `<h3>${projects[i].technologiesUsed}</h3>`;
+        newString +=    `<h3><a href="${projects[i].url}">URL GOES HERE</a></h3>`;
+        newString +=    `<h3><a href="${projects[i].githubUrl}">GitHub Link HERE</a></h3>`;
+        newString += `</div>`;
+        }
+    };
+    printToDom(newString, 'projectsPage');
+};
+
+createProjectCards();
+
+
+
+
+
