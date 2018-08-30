236b13ddf2f36098b85f63950906406b80747daa
Wayne Collier <ke4tri@gmail.com>
2018-08-30 14:44:55 -0500

    Everything is in the center and .css'd

--- projects.js
+++ projects.js
@@ -1,11 +1,22 @@
 const projects = 
 [
     {
+    project: "Wayne Collier",
     title: "Cool Project", 
     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
     available: true,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+},
+{
+    project: "Just an extra for testing the true/false",
+    story: "Cool Project", 
+    site: "http://gotoflashgames.com/files/file/033.jpg", 
+    about: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    techToTake: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: false,
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
 }
